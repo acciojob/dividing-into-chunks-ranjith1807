d4faff479ae80c91959dc74930cc4a39a0b05c32
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-03-25 21:01:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
 const divide = (arr, n) => {
   // Write your code here
+	let res=[]
+	let arr=[]
+	let sum=0;
+	for(let num of arr){
+		if(sum+num>n){
+			res.push(arr);
+			arr=[num]
+			sum=num
+		}
+		else{
+			arr.push(num)
+			sum+=num;
+			}
+	}
+	if (arr.length > 0) {
+    res.push(arr);
+  }
+  return res;
 };
 
 const n = prompt("Enter n: ");
